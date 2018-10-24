f06d6f83ae237aaaf103442a6dcaf79026a4362d
Toni Könnilä <toni.konnila@siili.com>
2018-10-24 20:15:35 +0300

    fixed tests

--- test/compojure_app/handler_test.clj
+++ test/compojure_app/handler_test.clj
@@ -7,8 +7,7 @@
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (str/starts-with? (:body response) "Hello World"))))
+      (is (= (:status response) 302))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
