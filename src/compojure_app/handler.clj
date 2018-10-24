c203c9db3dd6823883834435ddfd9b1fcf3277a0
Toni Könnilä <toni.konnila@siili.com>
2018-10-24 20:12:28 +0300

    added static resources

--- src/compojure_app/handler.clj
+++ src/compojure_app/handler.clj
@@ -1,10 +1,12 @@
 (ns compojure-app.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.util.response :as resp]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World!")
+  (route/resources "/")
+  (GET "/" [] (resp/redirect "/index.html"))
   (route/not-found "Not Found"))
 
 (def app
